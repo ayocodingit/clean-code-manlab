91f997c9dd4ae2fa20eacd898d933b0f119b80e2
ayocodingit <ayocodingit@gmail.com>
2021-01-30 13:40:49 +0700

    register mandiri

--- app/Http/Controllers/RegistrasiMandiri.php
+++ app/Http/Controllers/RegistrasiMandiri.php
@@ -35,147 +35,18 @@ class RegistrasiMandiri extends Controller
 
     public function getData(Request $request, $isData = false)
     {
-        $models = PasienRegister::leftJoin('register', 'register.id', 'pasien_register.register_id')
-            ->leftJoin('pasien', 'pasien.id', 'pasien_register.pasien_id')
-            ->leftJoin('fasyankes', 'fasyankes.id', 'register.fasyankes_id')
-            ->leftJoin('kota', 'kota.id', 'pasien.kota_id')
-            ->leftJoin('provinsi', 'pasien.provinsi_id', 'provinsi.id')
-            ->leftJoin('kecamatan', 'pasien.kecamatan_id', 'kecamatan.id')
-            ->leftJoin('kelurahan', 'pasien.kelurahan_id', 'kelurahan.id')
-            ->leftJoin('sampel', 'sampel.register_id', 'register.id')
-            ->where('sampel.is_from_migration', false)
-            ->where('pasien.is_from_migration', false)
-            ->where('pasien_register.is_from_migration', false)
-            ->where('register.is_from_migration', false)
-            ->whereNull('sampel.deleted_at')
-            ->whereNull('register.deleted_at');
+        $models = $this->query();
 
         $params = $request->get('params', false);
         $order = $request->get('order', 'name');
+        $order_direction = $request->get('order_direction', 'asc');
 
         if ($params) {
-            foreach (json_decode($params) as $key => $val) {
-                if ($val == '') {
-                    continue;
-                }
-
-                switch ($key) {
-                    case "nama_pasien":
-                        $models = $models->where('pasien.nama_lengkap', 'ilike', '%' . $val . '%');
-                        break;
-                    case "nomor_register":
-                        $models = $models->where('register.nomor_register', 'ilike', '%' . $val . '%');
-                        break;
-                    case "nomor_sampel":
-                        $sampel = Sampel::where('nomor_sampel', 'ilike', $val)->pluck('register_id');
-                        $models = $models->whereIn('register.id', $sampel);
-                        break;
-                    case "start_date":
-                        $models = $models->where('register.created_at', '>=', substr($val, 0, 10) . ' 00:00:00');
-                        break;
-                    case "end_date":
-                        $models = $models->where('register.created_at', '<=', substr($val, 0, 10) . ' 23:59:59');
-                        break;
-                    case "sumber_pasien":
-                        $models = $models->where('register.sumber_pasien', $val);
-                        break;
-                    case "sumber_sampel":
-                        $models = $models->where('register.nama_rs', $val);
-                        break;
-                    case "nama_rs":
-                        $models = $models->where("register.nama_rs", 'ilike', '%' . $val . '%');
-                        break;
-                    case "nama_rs_id":
-                        $models = $models->where("register.fasyankes_id", $val);
-                        break;
-                    case "nama_rs_lainnya":
-                        $models = $models->where("register.other_nama_rs", 'ilike', '%' . $val . '%');
-                        break;
-                    case "kota":
-                        $models = $models->where('kota.id', $val);
-                        break;
-                    case "kategori":
-                        if ($val != 'isian') {
-                            $models = $models->where('register.sumber_pasien', 'ilike', "%$val%");
-                        }
-                        break;
-                    case "kategori_isian":
-                        $models = $models->where('register.sumber_pasien', 'ilike', "%$val%");
-                        break;
-                    case "start_nomor_sampel":
-                        if (preg_match('{^' . Sampel::NUMBER_FORMAT . '$}', $val)) {
-                            $str = $val;
-                            $n = 1;
-                            $start = $n - strlen($str);
-                            $str1 = substr($str, $start);
-                            $str2 = substr($str, 0, $n);
-                            $models->whereRaw("sampel.nomor_sampel ilike '%$str2%'");
-                            $models->whereRaw("right(sampel.nomor_sampel,-1)::bigint >=  $str1");
-                        } else {
-                            $models->whereNull('sampel.nomor_sampel');
-                        }
-                        break;
-                    case "end_nomor_sampel":
-                        if (preg_match('{^' . Sampel::NUMBER_FORMAT . '$}', $val)) {
-                            $str = $val;
-                            $n = 1;
-                            $start = $n - strlen($str);
-                            $str1 = substr($str, $start);
-                            $str2 = substr($str, 0, $n);
-                            $models->whereRaw("sampel.nomor_sampel ilike '%$str2%'");
-                            $models->whereRaw("right(sampel.nomor_sampel,-1)::bigint <=  $str1");
-                        } else {
-                            $models->whereNull('sampel.nomor_sampel');
-                        }
-                        break;
-                    case "reg_fasyankes_pengirim":
-                        $models = $models->where('register.fasyankes_pengirim', 'ilike', $val);
-                        break;
-                    case "reg_fasyankes_id":
-                        break;
-                    default:
-                        $models = $models->where($key, $val);
-                        break;
-                }
-            }
+            $models = $this->params($params, $models);
         }
 
         if ($order) {
-            $order_direction = $request->get('order_direction', 'asc');
-            if (empty($order_direction)) {
-                $order_direction = 'desc';
-            }
-
-            switch ($order) {
-                case 'nama_lengkap':
-                case 'nama_pasien':
-                    $models = $models->orderBy('pasien.nama_lengkap', $order_direction);
-                    break;
-                case 'created_at':
-                case 'tgl_input':
-                    $models = $models->orderBy('register.created_at', $order_direction);
-                    break;
-                case 'nomor_register':
-                    $models = $models->orderBy('register.nomor_register', $order_direction);
-                    break;
-                case 'nama_kota':
-                    $models = $models->orderBy('kota.nama', $order_direction);
-                    break;
-                case 'sumber_pasien':
-                    $models = $models->orderBy('register.sumber_pasien', $order_direction);
-                    break;
-                case 'nama_rs':
-                    $models = $models->orderBy('register.nama_rs', $order_direction);
-                    break;
-                case 'no_sampel':
-                    $models = $models->orderBy('sampel.nomor_sampel', $order_direction);
-                    break;
-                case 'status':
-                    $models = $models->orderBy('pasien.status', $order_direction);
-                    break;
-                default:
-                    break;
-            }
+            $order = $this->order($order, $order_direction, $models);
         }
 
         $count = $models->count('register.nomor_register');
@@ -284,6 +155,143 @@ class RegistrasiMandiri extends Controller
         $no = (int)($request->get('page', 1) - 1) * $request->get('perpage', 500) + 1;
         foreach ($models as $idx => &$model) {
             $model->no = $no++;
+        }
+        return $models;
+    }
+
+    public function query()
+    {
+        return PasienRegister::leftJoin('register', 'register.id', 'pasien_register.register_id')
+        ->leftJoin('pasien', 'pasien.id', 'pasien_register.pasien_id')
+        ->leftJoin('fasyankes', 'fasyankes.id', 'register.fasyankes_id')
+        ->leftJoin('kota', 'kota.id', 'pasien.kota_id')
+        ->leftJoin('provinsi', 'pasien.provinsi_id', 'provinsi.id')
+        ->leftJoin('kecamatan', 'pasien.kecamatan_id', 'kecamatan.id')
+        ->leftJoin('kelurahan', 'pasien.kelurahan_id', 'kelurahan.id')
+        ->leftJoin('sampel', 'sampel.register_id', 'register.id')
+        ->where('sampel.is_from_migration', false)
+        ->where('pasien.is_from_migration', false)
+        ->where('pasien_register.is_from_migration', false)
+        ->where('register.is_from_migration', false)
+        ->whereNull('sampel.deleted_at')
+        ->whereNull('register.deleted_at');
+    }
+
+    public function params($params, $models)
+    {
+        foreach (json_decode($params) as $key => $val) {
+            if ($val == '') {
+                continue;
+            }
+
+            switch ($key) {
+                case "nama_pasien":
+                    $models = $models->where('pasien.nama_lengkap', 'ilike', '%' . $val . '%');
+                    break;
+                case "nomor_register":
+                    $models = $models->where('register.nomor_register', 'ilike', '%' . $val . '%');
+                    break;
+                case "nomor_sampel":
+                    $sampel = Sampel::where('nomor_sampel', 'ilike', $val)->pluck('register_id');
+                    $models = $models->whereIn('register.id', $sampel);
+                    break;
+                case "start_date":
+                    $models = $models->where('register.created_at', '>=', substr($val, 0, 10) . ' 00:00:00');
+                    break;
+                case "end_date":
+                    $models = $models->where('register.created_at', '<=', substr($val, 0, 10) . ' 23:59:59');
+                    break;
+                case "sumber_pasien":
+                    $models = $models->where('register.sumber_pasien', $val);
+                    break;
+                case "sumber_sampel":
+                    $models = $models->where('register.nama_rs', $val);
+                    break;
+                case "nama_rs":
+                    $models = $models->where("register.nama_rs", 'ilike', '%' . $val . '%');
+                    break;
+                case "nama_rs_id":
+                    $models = $models->where("register.fasyankes_id", $val);
+                    break;
+                case "nama_rs_lainnya":
+                    $models = $models->where("register.other_nama_rs", 'ilike', '%' . $val . '%');
+                    break;
+                case "kota":
+                    $models = $models->where('kota.id', $val);
+                    break;
+                case "kategori":
+                    if ($val != 'isian') {
+                        $models = $models->where('register.sumber_pasien', 'ilike', "%$val%");
+                    }
+                    break;
+                case "kategori_isian":
+                    $models = $models->where('register.sumber_pasien', 'ilike', "%$val%");
+                    break;
+                case "start_nomor_sampel":
+                    if (preg_match('{^' . Sampel::NUMBER_FORMAT . '$}', $val)) {
+                        $str = $val;
+                        $n = 1;
+                        $start = $n - strlen($str);
+                        $str1 = substr($str, $start);
+                        $str2 = substr($str, 0, $n);
+                        $models->whereRaw("sampel.nomor_sampel ilike '%$str2%'");
+                        $models->whereRaw("right(sampel.nomor_sampel,-1)::bigint >=  $str1");
+                    } else {
+                        $models->whereNull('sampel.nomor_sampel');
+                    }
+                    break;
+                case "end_nomor_sampel":
+                    if (preg_match('{^' . Sampel::NUMBER_FORMAT . '$}', $val)) {
+                        $str = $val;
+                        $n = 1;
+                        $start = $n - strlen($str);
+                        $str1 = substr($str, $start);
+                        $str2 = substr($str, 0, $n);
+                        $models->whereRaw("sampel.nomor_sampel ilike '%$str2%'");
+                        $models->whereRaw("right(sampel.nomor_sampel,-1)::bigint <=  $str1");
+                    } else {
+                        $models->whereNull('sampel.nomor_sampel');
+                    }
+                    break;
+                case "reg_fasyankes_pengirim":
+                    $models = $models->where('register.fasyankes_pengirim', 'ilike', $val);
+                    break;
+            }
+        }
+        return $models;
+    }
+
+    public function order($order, $order_direction, $models)
+    {
+        switch ($order) {
+            case 'nama_lengkap':
+            case 'nama_pasien':
+                $models = $models->orderBy('pasien.nama_lengkap', $order_direction);
+                break;
+            case 'created_at':
+            case 'tgl_input':
+                $models = $models->orderBy('register.created_at', $order_direction);
+                break;
+            case 'nomor_register':
+                $models = $models->orderBy('register.nomor_register', $order_direction);
+                break;
+            case 'nama_kota':
+                $models = $models->orderBy('kota.nama', $order_direction);
+                break;
+            case 'sumber_pasien':
+                $models = $models->orderBy('register.sumber_pasien', $order_direction);
+                break;
+            case 'nama_rs':
+                $models = $models->orderBy('register.nama_rs', $order_direction);
+                break;
+            case 'no_sampel':
+                $models = $models->orderBy('sampel.nomor_sampel', $order_direction);
+                break;
+            case 'status':
+                $models = $models->orderBy('pasien.status', $order_direction);
+                break;
+            default:
+                break;
         }
         return $models;
     }
